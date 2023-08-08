e746a6be65945225c368c72f86cb589c7acfeb8c
luoyuxia <luoyuxia@alumni.sjtu.edu.cn>
2023-08-08 16:21:58 +0800

    init benchmark for async generating cdc

---
+++ rocksdb/src/main/java/site/ycsb/db/rocksdb/LogAgent.java
@@ -0,0 +1,173 @@
+package site.ycsb.db.rocksdb;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.input.Tailer;
+import org.apache.commons.io.input.TailerListenerAdapter;
+import org.rocksdb.*;
+import site.ycsb.measurements.Measurements;
+import site.ycsb.workloads.CoreWorkload;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+
+/** Agent for WAL LOG to produce cdc log.*/
+public class LogAgent {
+
+  private static final String STATISTIC = "/tmp/statistic";
+  private static final int SLEEP = 10;
+  private final WALLogListener walLogListener;
+  private final Tailer tailer;
+  private final RocksDB rocksDB;
+
+  public LogAgent(String walLog, String cdcLog, RocksDB rocksDB,
+                  ConcurrentMap<String, RocksDBClient.ColumnFamily> columnFamilies) {
+    walLogListener = new WALLogListener(cdcLog, rocksDB, columnFamilies);
+    tailer = Tailer.create(new File(walLog), walLogListener, SLEEP);
+    this.rocksDB = rocksDB;
+  }
+
+  public void close() throws Exception {
+    if (tailer != null) {
+      tailer.stop();
+    }
+    if (walLogListener != null) {
+      walLogListener.close();
+    }
+
+    try(FileWriter fileWriter = new FileWriter(STATISTIC)) {
+      fileWriter.write(rocksDB.getProperty("rocksdb.stats"));
+    }
+  }
+
+  public boolean needWait() {
+    return walLogListener.needWait();
+  }
+
+  public boolean isFinished() {
+    return walLogListener.isFinished();
+  }
+
+  private static final class WALLogListener extends TailerListenerAdapter {
+
+    private static final int MAX_ALLOW_LATENCY_MILLS = 1_000;
+    private final FileOutputStream cdcOutputStream;
+    private final ObjectMapper objectMapper;
+    private final RocksDB rocksDB;
+    private final ReadOptions readOptions = new ReadOptions();
+    private  ColumnFamilyHandle targetColumnFamilyHandle;
+    private RocksDBClient.ColumnFamily columnFamily;
+    private final ConcurrentMap<String, RocksDBClient.ColumnFamily> columnFamilies;
+    private final ByteBuffer buffer = ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN);
+    private  boolean logAgentIsFinish;
+
+    private boolean cdcIsLate;
+    private WALLogListener(String cdcLog, RocksDB rocksDB,
+                           ConcurrentMap<String, RocksDBClient.ColumnFamily> columnFamilies) {
+      try{
+        cdcOutputStream = new FileOutputStream(cdcLog);
+        objectMapper = new ObjectMapper();
+        objectMapper.getFactory().disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+        this.columnFamilies = columnFamilies;
+        this.rocksDB = rocksDB;
+      } catch (Exception e) {
+        throw new RuntimeException("Fail to create WAL log listener.", e);
+      }
+    }
+
+    @Override
+    public void handle(String line) {
+      if (line.isEmpty()) {
+        logAgentIsFinish = true;
+        return;
+      }
+      if (targetColumnFamilyHandle == null) {
+        columnFamily = columnFamilies.get(CoreWorkload.TABLENAME_PROPERTY_DEFAULT);
+        targetColumnFamilyHandle = columnFamily.getHandle();
+        // use default column since we needs to get the file level statistic
+        targetColumnFamilyHandle = rocksDB.getDefaultColumnFamily();
+      }
+      try{
+        JsonNode jsonNode =  objectMapper.readTree(line);
+        // get the timestamp while writing
+        byte[] timestamp = jsonNode.get("timestamp").binaryValue();
+        long timestampLong = getTimeStamp(timestamp);
+        // get the update after value
+        byte[] updateAfter = jsonNode.get("updateAfter").binaryValue();
+
+        // get the key
+        byte[] key = jsonNode.get("key").binaryValue();
+
+        // try to get update before, we need to use timestampL - 1.
+        // if we use timestampL, we will get update after value
+        readOptions.setTimestamp(new Slice(toTimestamp(timestampLong - 1)));
+        long startTs = System.nanoTime();
+        final byte[] updateBefore = rocksDB.get(targetColumnFamilyHandle, readOptions, key);
+        long endTs = System.nanoTime();
+        Measurements.getMeasurements().measure("reading_for_updating",
+            (int) ((endTs - startTs) / 1000));
+
+        // write the log to cdc
+        startTs = System.nanoTime();
+        writeUpdateToCDCLog(key, updateBefore, updateAfter);
+        endTs = System.nanoTime();
+        Measurements.getMeasurements().measure("writing_to_cdc",
+            (int) ((endTs - startTs) / 1000));
+
+        // now, get the latency for from update to generating cdc
+        long writeTimestamp = timestampLong / 1_000_000;
+        long latency = System.currentTimeMillis() - writeTimestamp;
+//        cdcIsLate = latency > MAX_ALLOW_LATENCY_MILLS;
+        Measurements.getMeasurements().measure("cdc_generate", (int) (latency * 1000));
+      } catch (Exception e) {
+        throw new RuntimeException("Fail to handle the line.", e);
+      }
+    }
+
+    private void writeUpdateToCDCLog(byte[] key,
+                                     byte[] updateBeforeValues,
+                                     byte[] updateAfterValues) throws IOException {
+      Map<String, Object> map = new HashMap<>();
+      map.put("key", key);
+      map.put("updateBefore", updateBeforeValues);
+      map.put("updateAfter", updateAfterValues);
+      objectMapper.writeValue(cdcOutputStream, map);
+      // flush instead of sync
+      cdcOutputStream.flush();
+     // cdcOutputStream.getFD().sync();
+    }
+
+    public boolean isFinished() {
+      return logAgentIsFinish;
+    }
+
+    public boolean needWait() {
+      return cdcIsLate;
+    }
+
+
+    private void close() throws Exception {
+      if (cdcOutputStream != null) {
+        cdcOutputStream.close();
+      }
+    }
+
+    private long getTimeStamp(byte[] bytes) {
+      buffer.clear();
+      buffer.put(bytes);
+      buffer.flip();
+      return buffer.getLong();
+    }
+
+    private byte[] toTimestamp(long value) {
+      buffer.clear();
+      buffer.putLong(value);
+      return buffer.array();
+    }
+  }
+}
